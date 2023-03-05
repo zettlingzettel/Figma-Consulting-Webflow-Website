54a1dcd8d7b88286482907ef008f7bffa60055a2
ANNA SMOLENOVA <blare-lambdas-0t@icloud.com>
2023-03-05 18:06:15 -0500

    navbar text

--- src/components/homepage1/HomePage1NavBar.js
+++ src/components/homepage1/HomePage1NavBar.js
@@ -3,8 +3,27 @@ import React from 'react'
 const HomePage1NavBar = () => {
 
   return (
-    <div>Navbar
-    </div>
+      <nav>
+        <div className="navbar-topbar">
+          <p className="navbar-topbar-address">
+            24 Olive Street, Prairie, NY 53590
+          </p>
+          <a className="navbar-topbar-email-link">
+            whitecollar@gmail.com
+          </a>
+          <p className="navbar-topbar-working-hours">
+            Mon - Fri: 8:00 am - 5:00 pm
+          </p>
+        </div>
+        <div className="navbar-header">
+          <a>Home</a>
+          <a>About Us</a>
+          <a>Pages</a>
+          <a>Projects</a>
+          <a>Contact us</a>
+          <a><button>Get Consulting</button></a>
+        </div>
+      </nav>
   )
 }
 
